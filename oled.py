cd7a3ed8dd79c3287d81e7ccbbc00ff61ec80f62
unknown <ianperry99@gmail.com>
2020-01-04 21:43:44 -0500

    update

--- oled.py
+++ oled.py
@@ -12,7 +12,7 @@ from PIL import Image, ImageColor, ImageDraw, ImageFont
 def Test_Text():
     image = Image.new("RGB", (OLED.SSD1351_WIDTH, OLED.SSD1351_HEIGHT), "BLACK")
     draw = ImageDraw.Draw(image)
-    gps = "gps-green.bmp"
+    gps = Image.open("gps-green.bmp")
     draw.bitmap((20, 20), gps)
     font1 = ImageFont.truetype('notomono.ttf', 20)
     font2 = ImageFont.truetype('notomono.ttf', 14)
