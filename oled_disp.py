56980de844ddb8a8b11e07f058c72bb45bbdfb21
unknown <ianperry99@gmail.com>
2020-01-05 22:45:55 -0500

    updatep

--- oled_disp.py
+++ oled_disp.py
@@ -9,37 +9,36 @@ from luma.core.render import canvas
 from PIL import ImageFont
 
 
-def make_font(name, size):
-    font_path = os.path.abspath(os.path.join(os.path.dirname('/opt/pi-oled/'), 'fonts', name))
-    return ImageFont.truetype(font_path, size)
+def main():
+    def make_font(name, size):
+        font_path = os.path.abspath(os.path.join(os.path.dirname('/opt/pi-oled/'), 'fonts', name))
+        return ImageFont.truetype(font_path, size)
 
-noto12 = make_font("notomono.ttf", 12)
-noto14 = make_font("notomono.ttf", 14)
-noto16 = make_font("notomono.ttf", 16)
-noto20 = make_font("notomono.ttf", 20)
-fas = make_font("fa-solid-900.ttf", 25)
-fab = make_font("fa-brands-400.ttf", 25)
-far = make_font("fa-regular-400.ttf", 25)
+    noto12 = make_font("notomono.ttf", 12)
+    noto14 = make_font("notomono.ttf", 14)
+    noto16 = make_font("notomono.ttf", 16)
+    noto20 = make_font("notomono.ttf", 20)
+    fas = make_font("fa-solid-900.ttf", 25)
+    fab = make_font("fa-brands-400.ttf", 25)
+    far = make_font("fa-regular-400.ttf", 25)
 
 
-def get_display():
-    parser = cmdline.create_parser(description='luma.examples arguments')
-    try:
-        config = cmdline.load_config('/etc/oled.conf')
-        args = parser.parse_args(config)
-    except:
-        log.exception('No display configuration found (/etc/oled.conf)')
-        exit(1)
-    try:
-        device = cmdline.create_device(args)
-    except error.Error as e:
-        parser.error(e)
-        exit(1)
-    else:
+    def get_display():
+        parser = cmdline.create_parser(description='luma.examples arguments')
+        try:
+            config = cmdline.load_config('/etc/oled.conf')
+            args = parser.parse_args(config)
+        except:
+            log.exception('No display configuration found (/etc/oled.conf)')
+            exit(1)
+        try:
+            device = cmdline.create_device(args)
+        except error.Error as e:
+            parser.error(e)
+            exit(1)
+        else:
         return device
 
-
-def main():
     device = get_display()
     while True:
         with canvas(device) as draw:
@@ -103,9 +102,6 @@ def main():
             else:
                 fcolor = "yellow"
             draw.text((110, 0), text="\uf2ca", font=fas, fill=fcolor)
-
-
-
             draw.text((0, 27), text=f" {datetime.now().strftime('%H:%M:%S')}", font=noto20, fill="white")
         sleep(1)
 
